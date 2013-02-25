af2526b76e0d30e33d9a5b9d7a0a4607b1664f37
RoxanaPopescu <popescu.roxanna@gmail.com>
2013-02-25 08:00:39 +0100

    more changes to the class

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -5,7 +5,7 @@ public class SomeClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Testing Git Bash and GitHub.");
+		System.out.println("Testing Git Bash and GitHub. I like when it works");
 
 	}
 
